dad9b9f370b709a961912653d78117c2703baaba
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-02-08 23:04:30 +0800

    added MindMap type def

--- src/main.elm
+++ src/main.elm
@@ -1,14 +1,48 @@
 module Main exposing (..)
 
-import Html exposing (beginnerProgram, text)
+import Html exposing (beginnerProgram, text, div, h1, Attribute)
 
 
+type alias Node =
+    { id : String
+    , text : String
+    , node :
+        List
+            { id : String
+            , text : String
+            }
+    }
+
+
+type alias MindMap =
+    { map :
+        { version : String
+        , node : List Node
+        }
+    }
+
+
+type alias Model =
+    { title : String
+    , mindMap : MindMap
+    }
+
+
+init : Model
 init =
-    "init"
+    { title = "title"
+    , mindMap =
+        { map =
+            { version = "1"
+            , node = []
+            }
+        }
+    }
 
 
 view model =
-    text model
+    div []
+        [ h1 [] [ text model.title ] ]
 
 
 update msg model =
